7e498f2a493ce90dd0f61508047d5e18a95a1ea9
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2023-06-25 02:20:00 +0200

    More boilerplace things

---
+++ src/help.rs
@@ -0,0 +1,15 @@
+use kagero::printer::{Colors, Printer};
+
+/// Help Message
+/// 
+/// Displays a help message for using this tool.
+pub(crate) fn help_message(p: &mut Printer) {
+	p.writeln("Help Message still WIP");
+}
+
+/// Version Message
+/// 
+/// Prints metadata like authors and version of the program.
+pub(crate) fn version(p: &mut Printer) {
+	p.writeln("Version Message still WIP");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::process::ExitCode;
+use kagero::printer::Printer;
+mod help;
+
+fn main() -> ExitCode {
+	let mut prnt = Printer::default();
+	let args: Vec<String> = env::args().collect();
+
+	// Get file via command-line arguments
+	let Some(file) = args.get(1) else {
+		help::help_message(&mut prnt);
+		return ExitCode::FAILURE;
+	};
+
+	// Flag handle
+	return match file.as_str() {
+		"-h" | "--help" => {
+			help::help_message(&mut prnt);
+			ExitCode::SUCCESS
+		},
+		"-V" | "--version" => {
+			help::version(&mut prnt);
+			ExitCode::SUCCESS
+		},
+		_ => ExitCode::SUCCESS // TODO: Replace this with function call that does the acutal logic.
+	}
 }
