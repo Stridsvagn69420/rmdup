13a3ac74be425b95c166bc198e59e1f2eb5303f8
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2023-06-25 23:17:33 +0200

    Just added one second more runtime

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn main() -> ExitCode {
 		_ => {
 			// Convert to Path
 			let filepath = Path::new(file);
-			if !filepath.exists() || !filepath.is_file() {
+			if !filepath.is_file() {
 				p.error(file, Colors::RedBright).errorln(" does not exist or is not a file!", Colors::Red);
 				return ExitCode::FAILURE;
 			}
